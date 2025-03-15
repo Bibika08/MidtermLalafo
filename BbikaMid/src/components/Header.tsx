f5c6004cc59620031019baf9c4becad216fda96c
Student <student@compass.local>
2025-03-15 13:49:40 +0600

    запуск

---
+++ BbikaMid/src/components/Header.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../AxiosApi";
+import { AppBar, Box, Button, Container, Toolbar, Typography} from "@mui/material";
+
+const Header = () => {
+    const [categories, setCategories] = useState<{ id: string; name: string }[]>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await api.get("categories.json");
+                if (response.data) {
+                    const formattedCategories = Object.entries(response.data).map(([id, value]: any) => ({
+                        id,
+                        ...value,
+                    }));
+                    setCategories(formattedCategories);
+                }
+            } catch (error) {
+                console.error("Ошибка загрузки категорий:", error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
+
+    return (
+        <AppBar position="sticky" sx={{ backgroundColor: "#1976d2" }}>
+            <Container maxWidth="xl">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                            Все объявления
+                        </Link>
+                    </Typography>
+
+                    {/* Desktop Menu for categories */}
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: "20px" }}>
+                        {categories.map((category) => (
+                            <Button key={category.id} color="inherit">
+                                <Link to={`/category/${category.id}`} style={{ color: "white", textDecoration: "none" }}>
+                                    {category.name}
+                                </Link>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: "20px" }}>
+                        <Button color="inherit">
+                            <Link to="/createCategory" style={{ color: "white", textDecoration: "none" }}>
+                                Создать категорию
+                            </Link>
+                        </Button>
+                        <Button color="inherit">
+                            <Link to="/createAd" style={{ color: "white", textDecoration: "none" }}>
+                                Создать объявление
+                            </Link>
+                        </Button>
+                    </Box>
+
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Header;
